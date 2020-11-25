60cdfe60b7321b5a382f3bd2d1aeb9618bff3f4b
takuya <takuya.ikeda@tri-ad.global>
2020-11-25 19:46:29 +0900

    add comment

--- demoPoseEst16.py
+++ demoPoseEst16.py
@@ -8,44 +8,57 @@ import kornia as kn
 
 device = 'cuda'
 
+# Model 3d points
 cube = loadmat('demo_data/cube.mat')
 pts3d_gt = torch.tensor(cube['pts3d'], device=device, dtype=torch.float)
-n = pts3d_gt.size(0)
+# tensor([[ 1., -1., -1.],
+#         [ 1.,  1., -1.],
+#         [-1.,  1., -1.],
+#         [-1., -1., -1.],
+#         [ 1., -1.,  1.],
+#         [ 1.,  1.,  1.],
+#         [-1.,  1.,  1.],
+#         [-1., -1.,  1.]], device='cuda:0')
+# shape: [8, 3]
+n = pts3d_gt.size(0)  # 8
+
+# Pose
 poses = loadmat('demo_data/poses.mat')
-# P = torch.tensor(poses['poses'][0],
-#                  device=device).view(1, 6)  # camera poses in angle-axis
+# poses['poses'].shape: (50, 6)
 P = torch.tensor(poses['poses'][0],
                  device=device).reshape(1, 6)  # camera poses in angle-axis
 # [[-1.9496,  1.9800,  0.1222,  0.0659, -0.0374,  4.6802]]
-q_gt = kn.angle_axis_to_quaternion(P[0, 0:3])
+q_gt = kn.angle_axis_to_quaternion(P[0, 0:3])  # quat
 
+# Intrinsic Parameter
 f = 300
 u = 128
 v = 128
 K = torch.tensor([[f, 0, u], [0, f, v], [0, 0, 1]],
                  device=device,
                  dtype=torch.float)
-# import pdb
-# pdb.set_trace()
+
+# Projection
 pts2d_gt = BPnP.batch_project(P, pts3d_gt, K)
 bpnp = BPnP.BPnP.apply
+
+# Model Setting
 ite = 2000
-
-# pts2d = pts2d_gt.clone() + 100*torch.randn_like(pts2d_gt)
-# pts2d.requires_grad_()
-# optimizer = torch.optim.SGD([{'params':pts2d}], lr=0.2)
-
 model = torchvision.models.vgg11()
 model.classifier = torch.nn.Linear(25088, n * 2)
 model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.000004)
 
+# Init Pose
 ini_pose = torch.zeros(1, 6, device=device)
 ini_pose[0, 5] = 99
 losses = []
+
+# Track something
 track_2d = np.empty([ite, n, 2])
 track_2d_pro = np.empty([ite, n, 2])
 
+# Realtime Ploting
 plt.figure()
 ax3 = plt.subplot(1, 3, 3)
 plt.plot(pts2d_gt[0, :, 0].clone().detach().cpu().numpy(),
@@ -68,6 +81,9 @@ for i in range(ite):
     pts2d = model(torch.ones(1, 3, 32, 32, device=device)).reshape(1, n, 2)
     track_2d[i, :, :] = pts2d.clone().cpu().detach().numpy()
     P_out = bpnp(pts2d, pts3d_gt, K, ini_pose)
+    # P_out -> P_6d[i, :] = torch.cat((angle_axis, T), dim=-1)
+    import pdb
+    pdb.set_trace()
     pts2d_pro = BPnP.batch_project(P_out, pts3d_gt, K)
 
     loss = ((pts2d_pro - pts2d_gt)**2).mean() + ((pts2d_pro - pts2d)**2).mean()
